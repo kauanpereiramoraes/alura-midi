d561355636d619f6a9eea93ae745de624168fea3
kauanpereiramoraes <106698831+kauanpereiramoraes@users.noreply.github.com>
2023-09-19 13:23:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-//Pom ---------------------------------------
 function tocaSomPom (idElementoAudio) {
 document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onlick = tocaSomPom;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classiList[1];
+    const idAudio = `#som_$(instrumento)'; //template string
+
+    tecla.onlick = function () {
+      tocaSom(idAudio);
+    }
+
+}
